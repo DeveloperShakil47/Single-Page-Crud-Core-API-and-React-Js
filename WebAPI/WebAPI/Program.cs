cb3cbc53ce47cf3268dae05ce13f076710cf20d3
ETL <ETL@DESKTOP-H8JK0LS>
2022-05-31 17:12:21 +0600

    Add project files.

---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionstring = builder.Configuration.GetConnectionString("DefaultCon"); 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DonationDBContext>(options => options.UseSqlServer(connectionstring));
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(options => options.WithOrigins("http://localhost:3000")
+.AllowAnyHeader()
+.AllowAnyMethod());
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
